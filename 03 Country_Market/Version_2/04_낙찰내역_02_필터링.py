38c56f59a1cc554bed699785d4ffb7cfcfd6fc12
987cyh <987cyh@gmail.com>
2023-08-20 21:27:19 +0900

    Version_2 : MySQL DB, Grafana

--- 03 Country_Market/Version_2/04_낙찰내역_02_필터링.py
+++ 03 Country_Market/Version_2/04_낙찰내역_02_필터링.py
@@ -4,6 +4,7 @@ import pandas as pd
 from sqlalchemy import create_engine
 engine = create_engine("mysql+pymysql://root:1234@localhost:3306/country_market", isolation_level='AUTOCOMMIT')
 #%%
+# dataframe
 df = pd.read_sql(f"SELECT * FROM country_market.분석_낙찰내역 where 입찰공고명 regexp '용역|사업' AND 입찰공고명 "
                  f"NOT regexp '유지보수|분석센터|분석모델 개발사업|모델개발 사업|재구축|플랫폼 구축|시스템 구축|ISMP|측량|국가 바이오|"
                  f"서비스 고도화|시스템 고도화|컨설팅|감리|운영|NGS|FEMS|UAV|인사관리|유지관리|국가바이오|"
